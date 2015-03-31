e2bbad8907d618059c61633f799dec0ec0b0f41e
Curtis Ott <ott_curtis@yahoo.com>
2015-03-30 22:03:03 -0700

    added scoring and fixed newGame button

--- js/tic_tac_controller.js
+++ js/tic_tac_controller.js
@@ -8,6 +8,7 @@ angular.
 		var scoreP1 = 0;
 		var scoreP2 = 0;
 		var gameOver = "no";
+		var moves = 0;
 		
 		self.addMove = addMove;
 		self.getWinner = getWinner;
@@ -23,17 +24,23 @@ angular.
 		
 
 		function addMove($index){
-			if (self.listOfBoxes[$index].image == "img/UBCS.png" || self.listOfBoxes[$index].image == "img/raccoon_city_stars.png"){
-				alert("Invalid Move");
+			if (gameOver == "yes") {
+				alert("Please start a new game!");
 			} else {
-				if (currentPlayer == "a"){
-					self.listOfBoxes[$index].image = "img/UBCS.png";
-					self.listOfBoxes[$index].player = "a";
-					currentPlayer = "b";
+				if (self.listOfBoxes[$index].image == "img/UBCS.png" || self.listOfBoxes[$index].image == "img/raccoon_city_stars.png"){
+					alert("Invalid Move");
 				} else {
-					self.listOfBoxes[$index].image = "img/raccoon_city_stars.png";
-					self.listOfBoxes[$index].player = "b";
-					currentPlayer = "a";
+					if (currentPlayer == "a"){
+						self.listOfBoxes[$index].image = "img/UBCS.png";
+						self.listOfBoxes[$index].player = "a";
+						currentPlayer = "b";
+						moves ++;
+					} else {
+						self.listOfBoxes[$index].image = "img/raccoon_city_stars.png";
+						self.listOfBoxes[$index].player = "b";
+						currentPlayer = "a";
+						moves ++;
+					};
 				};
 			};
 		};
@@ -48,81 +55,85 @@ angular.
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[6].player === "a" && self.listOfBoxes[7].player === "a" && self.listOfBoxes[8].player === "a") {
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[0].player === "a" && self.listOfBoxes[4].player === "a" && self.listOfBoxes[8].player === "a") {
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[2].player === "a" && self.listOfBoxes[5].player === "a" && self.listOfBoxes[8].player === "a") {
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[0].player === "a" && self.listOfBoxes[3].player === "a" && self.listOfBoxes[6].player === "a") {
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[1].player === "a" && self.listOfBoxes[4].player === "a" && self.listOfBoxes[7].player === "a") {
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[2].player === "a" && self.listOfBoxes[5].player === "a" && self.listOfBoxes[8].player === "a") {
 				self.scoreP1 ++;
 				alert("A wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[0].player === "b" && self.listOfBoxes[1].player === "b" && self.listOfBoxes[2].player === "b"){
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[3].player === "b" && self.listOfBoxes[4].player === "b" && self.listOfBoxes[5].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[6].player === "b" && self.listOfBoxes[7].player === "b" && self.listOfBoxes[8].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[0].player === "b" && self.listOfBoxes[4].player === "b" && self.listOfBoxes[8].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[2].player === "b" && self.listOfBoxes[5].player === "b" && self.listOfBoxes[8].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[0].player === "b" && self.listOfBoxes[3].player === "b" && self.listOfBoxes[6].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[1].player === "b" && self.listOfBoxes[4].player === "b" && self.listOfBoxes[7].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else if (self.listOfBoxes[2].player === "b" && self.listOfBoxes[5].player === "b" && self.listOfBoxes[8].player === "b") {
 				self.scoreP2 ++;
 				alert("B wins!");
 				currentPlayer = "a";
-				gameOver = true;
+				gameOver = "yes";
 			} else {
-				null;
-			}
-			
+				if (moves == 9) {
+					alert("This has ended in a Tie!");
+					gameOver = "yes";
+				} else {
+					null;
+				};
+			};			
 		};
 
 		function newGame(){
@@ -131,7 +142,10 @@ angular.
 					{image:"img/blank.gif", player: ""}, {image:"img/blank.gif", player: ""}, {image:"img/blank.gif", player: ""}, 
 					{image:"img/blank.gif", player: ""}, {image:"img/blank.gif", player: ""}, {image:"img/blank.gif", player: ""}, 
 					{image:"img/blank.gif", player: ""}, {image:"img/blank.gif", player: ""}, {image:"img/blank.gif", player: ""}
-					];	
+					];
+				gameOver = "no";
+				currentPlayer = "a"
+				moves = 0;	
 			} else {
 				null;
 			}; 
